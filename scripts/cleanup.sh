f6fe509494e8328794b2f0fd15bc857025f2f81f
Sean Newham <snewham@thoughtworks.com>
2016-01-04 11:48:35 +0000

    Spelling mistake

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,5 +1,5 @@
 if [ -z "${SERVICE_NAME}" ]; then
-  echo "Cleaning up local environemnt..."
+  echo "Cleaning up local environment..."
   docker rmi -f $(docker images | grep 'goberry' | awk {'print $3'} | uniq)
 else
   docker rmi -f $(docker images | grep ${SERVICE_NAME} | awk {'print $3'} | uniq)
